fd1f1c28bd310caeba86b5f615727ea99802960b
Louis Marx <louis.marx@mailo.com>
2022-11-06 12:02:06 +0100

    Clean Import

--- R/random-forest.R
+++ R/random-forest.R
@@ -1,11 +1,12 @@
 # install.packages("randomForest", repos = "http://cloud.r-project.org")
 library(randomForest)
-# detach(games)
+detach(games)
 
 
 ### Load data
 games <- readRDS(file = "data/games_restructured.rds")
 # games <- readRDS(file = "data/games_imputed.rds")
+print(str(games))
 attach(games)
 
 
@@ -19,64 +20,41 @@ attach(games)
 
 
 ### Regression with Random Forest
-games_wt <- ifelse(games$tournament == "FIFA World Cup", 3.5, 1)
-score.rf <- randomForest(team_score ~ ., data = games, ntree = 500, mtry = 3, importance = TRUE, na.action = na.omit, do.trace = TRUE)
-print(score.rf)
-print(round(importance(score.rf), 2))
-varImpPlot(score.rf)
-
-
-
-
-
-
-
-# Prepare training and test sets
-# set.seed(4)
-# train <- sample(1:nrow(Games), nrow(Games))
-# games.train <- data.frame(subset(Games[train, ], select = -c(team_score)))
-# games.test <- subset(Games[-train, ], select = -c(team_score))
-# score.train <- as.vector(unlist(data.frame(Games[train, "team_score"])))
-# score.test <- unlist(Games[-train, "team_score"])
-
-# Random forest modelling
-# Parameters to optimize:  mtry and ntree
-# tst <- tuneRF(games.train, score.train,
-#     improve = 0.0001, ntreeTry = 500, stepFactor = .5,
-#     trace = TRUE, plot = TRUE, doBest = FALSE
-# )
-# print(tst)
-# score.rf <- randomForest(team_score ~ ., data = Games, subset = train, keep.forest = TRUE, importance = TRUE, xtest = games.test, ytest = score.test)
-# score.rf <- randomForest(team_score ~ ., data = Games, ntree = 100, do.trace = TRUE)
-# plot(score.rf)
-# print(importance(score.rf))
+# games_wt <- ifelse(games$tournament == "FIFA World Cup", 3, 1)
+# score.rf <- randomForest(team_score ~ ., data = games, ntree = 500, mtry = 3, importance = TRUE, na.action = na.omit, do.trace = TRUE)
+# print(score.rf)
+# print(round(importance(score.rf), 2))
 # varImpPlot(score.rf)
-# r2pmml(rf.games, "models/random_forest.pmml")
 
-detach(games)
 
-# teams <- read.csv("data/teams.csv", colClasses = c("character", "character", "character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
-# teams <- mutate(teams, group = NULL)
+# detach(games)
 
-# tournament <- data.frame(tournament = "FIFA World Cup", country = "Qatar")
 
-# xgoal <- matrix(0, ncol = nrow(teams), nrow = nrow(teams))
-# rownames(xgoal) <- teams$team_fifa_code
-# colnames(xgoal) <- teams$team_fifa_code
+teams <- read.csv("data/teams.csv", colClasses = c("character", "character", "character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+teams <- mutate(teams, group = NULL)
 
-# teams <- mutate(teams, team_fifa_code = NULL)
+tournament <- data.frame(tournament = "FIFA World Cup", country = "Qatar")
 
-# for (i in 1:nrow(teams)) {
-#     for (j in 1:nrow(teams)) {
-#         team <- teams[i, ]
-#         opponent <- rename_with(teams[j, ], ~ gsub("team", "opponent", .x))
-#         match <- bind_cols(team, opponent, tournament)
-#         # match <- mutate(match, across(c(team_continent, opponent_continent), as.factor))
-#         match <- mutate(match, team_hosting = team == country)
-#         match <- mutate(match, opponent_hosting = opponent == country)
-#         xgoal[i, j] <- predict(score.rf, match, type = "response")
-#     }
-# }
+xgoal <- matrix(0, ncol = nrow(teams), nrow = nrow(teams))
+rownames(xgoal) <- teams$team_fifa_code
+colnames(xgoal) <- teams$team_fifa_code
 
-# print(xgoal)
-# write.csv(xgoal, file = "data/xgoal.csv")
+teams <- mutate(teams, team_fifa_code = NULL)
+
+for (i in 1:nrow(teams)) {
+    for (j in 1:nrow(teams)) {
+        team <- teams[i, ]
+        opponent <- rename_with(teams[j, ], ~ gsub("team", "opponent", .x))
+        match <- bind_cols(team, opponent, tournament)
+        match <- mutate(match, team_hosting = team == country)
+        match <- mutate(match, opponent_hosting = opponent == country)
+        match <- mutate(match, fifa_rank_difference = opponent_fifa_rank - team_fifa_rank, .keep = "unused", .after = opponent_fifa_rank)
+        match <- mutate(match, fifa_points_difference = team_total_fifa_points - opponent_total_fifa_points, .keep = "unused", .after = opponent_total_fifa_points)
+        match <- mutate(match, across(c(team, opponent, team_continent, opponent_continent, tournament, country, team_hosting, opponent_hosting), as.factor))
+        print(str(match))
+        xgoal[i, j] <- predict(score.rf, match, type = "response")
+    }
+}
+
+print(xgoal)
+write.csv(xgoal, file = "data/xgoal.csv")
--- data-raw/import-data.R
+++ data-raw/import-data.R
@@ -3,7 +3,11 @@
 library("readxl")
 library("dplyr")
 
-### Load data
+
+
+
+
+################################################## Load data ##################################################
 
 # Load international games dataset
 international_games <- read_xlsx("data-raw/DA Challenge - Data International Soccer Games.xlsx",
@@ -18,47 +22,76 @@ international_games <- read_xlsx("data-raw/DA Challenge - Data International Soc
     )
 )
 
-# Load selected tournaments dataset
-selected_tournaments <- read.csv("data-raw/tournaments_selection.csv", colClasses = c("character", "logical"))
-selected_tournaments <- selected_tournaments[selected_tournaments$selected == TRUE, "tournaments"]
-
 # Load selected teams dataset
 selected_teams <- read.csv("data-raw/teams_selection.csv", colClasses = c("character"))
 selected_teams <- selected_teams[, "country"]
 
+###############################################################################################################
 
-### Create a version of the dataset with games from the home team perspective
+
+
+
+
+################## Create a version of the dataset with games from the home team perspective ##################
 
 # Rename columns from home team perspective
 home_team_perspective <- rename_with(international_games, ~ gsub("home_", "", .x))
 home_team_perspective <- rename_with(home_team_perspective, ~ gsub("away_team", "opponent", .x))
 
 # Replace neutral_location with team_hosting and opponent_hosting variables from home team perspective
-home_team_perspective <- mutate(home_team_perspective, team_hosting = !neutral_location, .keep = "unused", .after = neutral_location)
-home_team_perspective <- mutate(home_team_perspective, opponent_hosting = FALSE, .after = team_hosting)
+home_team_perspective <- mutate(home_team_perspective, team_hosting = !neutral_location, .keep = "unused")
+home_team_perspective <- mutate(home_team_perspective, opponent_hosting = FALSE)
 
 # Replace team_result with result variable from home team perspective
 home_team_perspective <- rename(home_team_perspective, result = team_result)
 
+###############################################################################################################
 
-### Create a version of the dataset with games from the away team perspective
+
+
+
+
+################## Create a version of the dataset with games from the away team perspective ##################
 
 # Rename columns from away team perspective
 away_team_perspective <- rename_with(international_games, ~ gsub("away_", "", .x))
 away_team_perspective <- rename_with(away_team_perspective, ~ gsub("home_team", "opponent", .x))
 
 # Replace neutral_location with team_hosting and opponent_hosting variables from away team perspective
-away_team_perspective <- mutate(away_team_perspective, team_hosting = FALSE, .after = neutral_location)
-away_team_perspective <- mutate(away_team_perspective, opponent_hosting = !neutral_location, .keep = "unused", .after = team_hosting)
+away_team_perspective <- mutate(away_team_perspective, team_hosting = FALSE)
+away_team_perspective <- mutate(away_team_perspective, opponent_hosting = !neutral_location, .keep = "unused")
 
 # Replace opponent_result with result variable from away team perspective
-away_team_perspective <- mutate(away_team_perspective, result = recode(opponent_result, Win = "Lose", Lose = "Win"), .keep = "unused", .after = opponent_result)
+away_team_perspective <- mutate(away_team_perspective, result = recode(opponent_result, Win = "Lose", Lose = "Win"), .keep = "unused")
+
+###############################################################################################################
 
 
-### Combine the two datasets generated into one with double the number of rows
+
+
+
+################# Combine the two datasets generated into one with double the number of rows ##################
 
 # Bind datasets from both perspectives
 international_games <- bind_rows(home_team_perspective, away_team_perspective)
+
+# Transform fifa rank and points variables into differences
+international_games <- mutate(international_games, fifa_rank_difference = opponent_fifa_rank - team_fifa_rank, .keep = "unused", .after = opponent_fifa_rank)
+international_games <- mutate(international_games, fifa_points_difference = team_total_fifa_points - opponent_total_fifa_points, .keep = "unused", .after = opponent_total_fifa_points)
+
+# Keep only selected teams games
+international_games <- filter(international_games, team %in% selected_teams & opponent %in% selected_teams & country %in% selected_teams)
+
+# Convert string and boolean variables as factors
+international_games <- mutate(international_games, across(c(team, opponent, team_continent, opponent_continent, tournament, city, country, team_hosting, opponent_hosting, shoot_out, result), as.factor))
+
+###############################################################################################################
+
+
+
+
+
+#######################  Finalize the international_games dataset for modelling purpose #######################
 
 # Rearrange rows in chronological order
 international_games <- arrange(international_games, date, city)
@@ -66,24 +99,13 @@ international_games <- arrange(international_games, date, city)
 # Drop useless variables for future modeling
 international_games <- mutate(international_games, opponent_score = NULL, date = NULL, result = NULL, shoot_out = NULL, city = NULL)
 
-# Transform fifa rank and points variables into differences
-international_games <- mutate(international_games, fifa_rank_difference = opponent_fifa_rank - team_fifa_rank, .keep = "unused", .after = opponent_fifa_rank)
-international_games <- mutate(international_games, fifa_points_difference = team_total_fifa_points - opponent_total_fifa_points, .keep = "unused", .after = opponent_total_fifa_points)
-
-# Keep only selected tournaments and teams games
-# international_games <- filter(international_games, tournament %in% selected_tournaments)
-international_games <- filter(international_games, team %in% selected_teams & opponent %in% selected_teams)
-international_games <- filter(international_games, country %in% selected_teams)
-
-# Convert string and boolean variables as factors
-international_games <- mutate(international_games, across(c(team, opponent, team_continent, opponent_continent, tournament, country, team_hosting, opponent_hosting), as.factor))
-
-
 # Convert tibble to dataframe
 international_games <- as.data.frame(unclass(international_games))
 
-# print(str(international_games))
-# View(Games)
+# Quick preview of the dataframe to check if everything's ok
+print(str(international_games))
 
-# Export the resultin dataset in a RDS format
+# Export the resulting dataset into a RDS format
 saveRDS(international_games, "data/games_restructured.rds")
+
+###############################################################################################################
